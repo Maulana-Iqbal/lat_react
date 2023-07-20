b3faabe1f19da8300255f3ed530842a036879f14
Maulana-Iqbal <maulanai957@gmail.com>
2023-07-20 11:14:56 +0700

    add avatar rounded, shadow

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,34 @@ import Button from '@mui/material/Button';
 // import { SvgIcon } from './assets/iqbal.png';
 // import SvgIcon from '@mui/material/react';
 import myImage from './assets/react.svg'; 
+import { Avatar } from '@mui/material';
+import Paper from '@mui/material/Paper';
 
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const styles = {
+    container: {
+      display: 'flex',
+      justifyContent: 'center', // Center horizontally
+      alignItems: 'center', // Center vertically
+    },
+
+    shadowContainer: {
+      boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.5)', // Customize the shadow as per your preference
+    },
+  };
+
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={IqbalLogo} className="logo" alt="Vite logo" />
-        </a>
+      <div style={styles.container}>
+        <Avatar alt="Iqbal Maulana"  src={IqbalLogo} sx={{ width: 100, height: 100 }} />
       </div>
       <h3>Iqbal Maulana</h3>
       <Stack spacing={2}>
-        <Button variant='outlined' onClick={() => window.location.replace('https://google.com')}>
+        <Button style={styles.shadowContainer} variant='outlined' onClick={() => window.location.replace('https://google.com')}>
           <Stack 
           direction="row"
           justifyContent="space-between"
@@ -32,7 +44,7 @@ function App() {
             <h5>icon</h5>
           </Stack>
         </Button>
-        <Button variant='outlined' onClick={() => window.location.replace('https://google.com')}>
+        <Button style={styles.shadowContainer} variant='outlined' onClick={() => window.location.replace('https://google.com')}>
           <Stack 
           direction="row"
           justifyContent="space-between"
@@ -44,7 +56,7 @@ function App() {
             <h5>icon</h5>
           </Stack>
         </Button>
-        <Button variant='outlined' onClick={() => window.location.replace('https://google.com')}>
+        <Button style={styles.shadowContainer} variant='outlined' onClick={() => window.location.replace('https://google.com')}>
           <Stack 
           direction="row"
           justifyContent="space-between"
